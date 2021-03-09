0ec903f7f829b151becabacd4424c3b8f20652ad
Vladislav-Main <vlados.77772@gmail.com>
2021-03-09 13:46:05 +0200

    HW-01: task2 has been done

---
+++ task2.js
@@ -0,0 +1,41 @@
+'use strict'
+
+//checkNumber
+checkNumber(7);
+checkNumber(-10);
+
+function checkNumber(value) {
+
+  let arr = [
+    isPrime(value),
+    isEven(value),
+    isMultiple(value)
+  ];
+  console.log(arr);
+    
+  function isPrime(n) {
+    if ( n > 1 && n % n == 0 && n % 1 == 0){
+      return true;
+    }else {
+      return false;
+    }
+    
+  }
+
+  function isEven(n) {
+    if ( n % 2 == 0) {
+        return true;
+      }else {
+        return false;
+      } 
+    
+  }
+
+  function isMultiple(n) {
+    if (n % 10 == 0){
+      return true;
+    }else {
+      return false;
+    }
+  }
+}
